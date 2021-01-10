546cd91ffd149ae921b0615191780165ad3ad7f1
Clarklee2009 <38969091+Clarklee2009@users.noreply.github.com>
2021-01-10 14:03:30 -0500

    version3

---
+++ static/js/auto_fill_and_geo.js
@@ -0,0 +1,62 @@
+function autoComplete (){
+	const input = document.getElementById("input");
+	autocomplete = new google.maps.places.Autocomplete(input);
+	autocomplete.setFields(["address_components", "geometry", "icon", "name"]);
+
+	autocomplete.addListener("place_changed", () => {
+		const place = autocomplete.getPlace();
+		console.log(place.name);
+		console.log(place.geometry);
+	});
+}
+// This sample requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script
+// src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBIwzALxUPNbatRBj3Xi1Uhp0fFzwWNBkE&libraries=places">
+// function autoComplete() {
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -33.8688, lng: 151.2195 },
+//     zoom: 13,
+//   });
+//   const input = document.getElementById("input");
+//   const autocomplete = new google.maps.places.Autocomplete(input);
+//   autocomplete.bindTo("bounds", map);
+//   // Specify just the place data fields that you need.
+//   autocomplete.setFields(["place_id", "geometry", "name", "formatted_address"]);
+//   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+//   const infowindow = new google.maps.InfoWindow();
+//   const infowindowContent = document.getElementById("infowindow-content");
+//   infowindow.setContent(infowindowContent);
+//   const geocoder = new google.maps.Geocoder();
+//   const marker = new google.maps.Marker({ map: map });
+//   marker.addListener("click", () => {
+//     infowindow.open(map, marker);
+//   });
+//   autocomplete.addListener("place_changed", () => {
+//     infowindow.close();
+//     const place = autocomplete.getPlace();
+
+//     if (!place.place_id) {
+//       return;
+//     }
+//     geocoder.geocode({ placeId: place.place_id }, (results, status) => {
+//       if (status !== "OK") {
+//         window.alert("Geocoder failed due to: " + status);
+//         return;
+//       }
+//       map.setZoom(11);
+//       map.setCenter(results[0].geometry.location);
+//       // Set the position of the marker using the place ID and location.
+//       marker.setPlace({
+//         placeId: place.place_id,
+//         location: results[0].geometry.location,
+//       });
+//       marker.setVisible(true);
+//       infowindowContent.children["place-name"].textContent = place.name;
+//       infowindowContent.children["place-id"].textContent = place.place_id;
+//       infowindowContent.children["place-address"].textContent =
+//         results[0].formatted_address;
+//       infowindow.open(map, marker);
+//     });
+//   });
+// }
